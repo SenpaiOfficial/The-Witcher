ee2ec59206242094bb1480283b0c71d735d3c1aa
SenpaiOfficial <senpai.makeyev@gmail.com>
2020-06-25 00:37:25 +0600

    feat: add modul window and js code for this

--- js/main.js
+++ js/main.js
@@ -1,7 +1,20 @@
 var mySwiper = new Swiper('.swiper-container', {
- slidesPerView: 2,
+ slidesPerView: 1,
  loop: true,
  navigation: {
   nextEl: '.arrow',
  },
+ breakpoints: {
+  540: {
+   slidesPerView: 2,
+  }
+ }
 });
+
+var menuButton = document.querySelector('.menu__button');
+var menu = document.querySelector('.header');
+
+menuButton.addEventListener('click', function () {
+ menuButton.classList.toggle('menu__button--active');
+ menu.classList.toggle('header--active');
+})
